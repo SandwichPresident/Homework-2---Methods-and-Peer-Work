115a8f6ac2fb13eac2717ad65d06b58831aa959c
Vicky <vickyn5104@gmail.com>
2025-10-02 08:46:07 -0400

    minor tweak

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -32,7 +32,7 @@ namespace ConsoleApp1
             Console.WriteLine("Sam's favorite number is " + samNum);
             Console.WriteLine("Vicky's favorite number is " + vickyNum);
 
-             int subtractNums(int num1, int num2)
+            int subtractNums(int num1, int num2)
             {
                 return num1 - num2;
             }
