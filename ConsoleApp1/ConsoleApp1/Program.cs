491822f85319d6e4010452ccae820519cdd7eff6
Vicky <vickyn5104@gmail.com>
2025-10-02 08:26:34 -0400

    added subtractNums function

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,6 @@
-﻿/* Student Names: Vicky Nguyen and Sam Booth
-IGME.201.01 - Interac Des & Alg Prob Sol III
-Due: 10/12/25
+﻿/* Student Names: Vicky Ni and Sam Booth
+ * IGME.201.01 - Interac Des & Alg Prob Sol III
+ * Due: 10/12/25
  */
 
 namespace ConsoleApp1
@@ -13,21 +13,21 @@ namespace ConsoleApp1
         {
             return num1 + num2;
         }
+
         static void Main(string[] args)
         {
             int samNum;
             int vickyNum;
 
-
-
-
             //Initialize
             samNum = 24;
             vickyNum = 4;
 
-
-
             //start of our actual program
+            private int subtractNums (int num1, int num2)
+            {
+                return num1 - num2;
+            }
         }
     }
 }
