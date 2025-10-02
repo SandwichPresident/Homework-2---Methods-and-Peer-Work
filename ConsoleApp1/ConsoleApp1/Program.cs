53bce4c07cc9dd8a75eb91b54bc35a430b17182f
Vicky <vickyn5104@gmail.com>
2025-10-02 08:28:22 -0400

    fixed location of substractNums

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -23,11 +23,13 @@ namespace ConsoleApp1
             samNum = 24;
             vickyNum = 4;
 
-            //start of our actual program
-            private int subtractNums (int num1, int num2)
+            private int subtractNums(int num1, int num2)
             {
                 return num1 - num2;
             }
+
+        //start of our actual program
+
         }
     }
 }
